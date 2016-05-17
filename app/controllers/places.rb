f760bec37d74a18b97638f553a204d09a12ca19b
Wayne Rumble <wrumble@Waynes-MacBook-Pro.local>
2016-05-17 11:21:44 +0100

    places now linked to users

--- app/controllers/places.rb
+++ app/controllers/places.rb
@@ -10,10 +10,12 @@ class Makersbnb < Sinatra::Base
   end
 
   post '/places' do
+    user = User.get(session[:user_id])
     Place.create(
       name: params[:name],
       description: params[:description],
-      price: params[:price].to_i)
+      price: params[:price].to_i,
+      user_id: user.id)
     redirect '/places'
   end
 
