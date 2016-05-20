ecfef45b23376b5769d5eb6d6340d5038a94ec0c
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-20 16:19:47 +0100

    Availability_spec now test objects, as well as page content

--- spec/feature/availability_spec.rb
+++ spec/feature/availability_spec.rb
@@ -6,6 +6,7 @@ feature 'Host can change availability on their places' do
     add_availability
     expect(current_path).to eq('/places/manage/availability')
     expect(page).to have_content('Start date: 2016-06-20')
+    expect(Availability.last.start_date.to_s).to eq '2016-06-20'
   end
 
   scenario 'Availabilities have a start date and end date' do
@@ -14,6 +15,8 @@ feature 'Host can change availability on their places' do
     add_availability
     expect(page).to have_content('Start date: 2016-06-20')
     expect(page).to have_content('End date: 2016-06-25')
+    expect(Availability.last.start_date.to_s).to eq '2016-06-20'
+    expect(Availability.last.end_date.to_s).to eq '2016-06-25'
   end
 
 end
