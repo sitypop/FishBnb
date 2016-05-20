e58327030d9912c4de23154fd50dd384a775f062
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-20 16:24:26 +0100

    Place_spec now tests object, as well as page content

--- spec/feature/place_spec.rb
+++ spec/feature/place_spec.rb
@@ -4,8 +4,8 @@ feature 'Creating a place' do
     sign_up
     add_place
     expect(current_path).to eq '/places'
+    expect(Place.first.name).to eq 'Downton Abbey'
     expect(Place.first.user_id).to eq User.first.id
-
     within 'ul#places' do
       expect(page).to have_content('Downton Abbey')
       expect(page).to have_content('Start date: 2016-06-13')
@@ -23,6 +23,7 @@ feature 'Creating a place' do
     click_button 'Accept?'
     click_button 'Back'
     expect(page).to have_content('This place is unavailable')
+    expect(Place.first.booked).to be true
   end
 
 end
